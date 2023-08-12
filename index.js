ad1ee38992ff085e56c50b50b64c88073df59786
Jesse Reynolds <jessereynolds18@gmail.com>
2023-08-11 23:04:31 -0500

    add an if statement, a ternary statement, and a switch statement

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let numberOfFeet = 199;
+
+function scuberGreetingForFeet(numberOfFeet){
+  if (numberOfFeet < 400) {
+    return 'This one is on me!';
+  } else if (numberOfFeet >= 400) {
+    if (numberOfFeet <= 2000) {
+      return 'That will be twenty bucks.';
+    }
+  }
+
+  if (numberOfFeet >= 2001 && numberOfFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  
+  if (numberOfFeet >= 2501) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city = 'NYC';
+let cityMessage;
+
+function ternaryCheckCity(city){
+  cityMessage = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tipSize;
+function switchOnCharmFromTip(tipSize) {
+  switch (tipSize) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
